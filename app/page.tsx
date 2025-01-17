e7f27cc873f2bc4176aac3625f42bbe3f1af0610
Senzso <110294817+Senzso@users.noreply.github.com>
2025-01-18 00:38:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ function HeroSection({ openTerminal }) {
               className="px-4 py-1 bg-white/10 text-white"
             >
               <span className="w-2 h-2 rounded-full bg-primary inline-block mr-2" />
-              BQb5CyNLscCStM9tv7CmLpyTD6BnqAqAyeGjhsmLpump
+              Multi-Chain
             </Badge>
           </div>
           <Button
